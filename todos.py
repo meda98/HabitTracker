d199e22d5404c5b8b2cf326bf1ccc0c35ddb5a4d
meda98 <melaniedarda98@gmail.com>
2024-06-24 07:56:02 +0200

    Delete todos.py

--- todos.py
+++
@@ -1,9 +0,0 @@
-## Next Steps
-
-# Read feedback again and ask questions (only one database ok?, do I need date AND time necessarily? etc.)
-# Build testing-file
-# Build PP presentation
-
-
-# weekly/daily-list
-# completion of habit: date AND time
